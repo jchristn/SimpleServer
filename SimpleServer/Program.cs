7042109bf8e4181d2d1832649d0f47fce76ea69d
Joel Christner <joel.christner@gmail.com>
2020-10-23 10:47:21 -0700

    Dependency update

--- SimpleServer/Program.cs
+++ SimpleServer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using WatsonWebserver;
 using Newtonsoft.Json;
 
@@ -6,17 +7,25 @@ namespace SimpleServer
 {
     class Program
     {
+        static string _Hostname = "localhost";
+        static int _Port = 8888;
+        static bool _Ssl = false;
+        static Server _Server = null;
+
         static void Main(string[] args)
-        {
-            Server s = new Server("localhost", 8888, false, DefaultRequest);
+        { 
+            _Server = new Server(_Hostname, _Port, _Ssl, DefaultRequest);
+            _Server.Start();
+            Console.WriteLine((_Ssl ? "https:" : "http:") + "//" + _Hostname + ":" + _Port);
             Console.WriteLine("Press ENTER to exit");
             Console.ReadLine();
         }
 
-        static HttpResponse DefaultRequest(HttpRequest req)
+        static async Task DefaultRequest(HttpContext ctx)
         {
-            Console.WriteLine(req.ToString());
-            return new HttpResponse(req, 200, null, "application/json", SerializeJson(req, true));
+            Console.WriteLine(SerializeJson(ctx.Request, true));
+            ctx.Response.ContentType = "application/json";
+            await ctx.Response.Send(SerializeJson(ctx.Request, true));
         }
 
         static string SerializeJson(object obj, bool pretty)
